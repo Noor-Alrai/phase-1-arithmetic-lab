50ae086f979576345b0501b6965794777af3e8e9
Noor-Alrai <ameinnoor91@gmail.com>
2023-09-23 13:34:51 +0300

    Complete lab

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
 // Write your code here
+let multiply=62;
+let num1=2, num2=31;
+(Number(num1) *Number(num2) ) == multiply;
+multiply;
+
+let random= Math.floor(Math.random()) + 1;
+let mod=4
+let num3=16 , num4=4;
+num3 % num4 == mod;
+mod;
+let max= (10,15 ,8 ,20);
+Math.max(max);
+
